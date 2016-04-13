591485fc52675e5209ff6a53626206daef8b5efe
Lineker Tomazeli <ltomazeli@iitreacts.com>
2016-04-13 10:27:04 -0400

    Added Abp.Zero project to the same sln as ASP.NET boilerplate

---
+++ src/Abp.Zero/Zero/AbpZeroCoreModule.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using Abp.Application.Features;
+using Abp.Dependency;
+using Abp.Localization;
+using Abp.Localization.Dictionaries;
+using Abp.Localization.Dictionaries.Xml;
+using Abp.Modules;
+using Abp.Zero.Configuration;
+using Castle.MicroKernel.Registration;
+
+namespace Abp.Zero
+{
+    /// <summary>
+    /// ABP zero core module.
+    /// </summary>
+    [DependsOn(typeof(AbpKernelModule))]
+    public class AbpZeroCoreModule : AbpModule
+    {
+        /// <summary>
+        /// Current version of the zero module.
+        /// </summary>
+        public const string CurrentVersion = "0.8.4.0";
+
+        public override void PreInitialize()
+        {
+            IocManager.Register<IRoleManagementConfig, RoleManagementConfig>();
+            IocManager.Register<IUserManagementConfig, UserManagementConfig>();
+            IocManager.Register<ILanguageManagementConfig, LanguageManagementConfig>();
+            IocManager.Register<IAbpZeroConfig, AbpZeroConfig>();
+
+            Configuration.Settings.Providers.Add<AbpZeroSettingProvider>();
+
+            Configuration.Localization.Sources.Add(
+                new DictionaryBasedLocalizationSource(
+                    AbpZeroConsts.LocalizationSourceName,
+                    new XmlEmbeddedFileLocalizationDictionaryProvider(
+                        Assembly.GetExecutingAssembly(), "Abp.Zero.Localization.Source"
+                        )));
+
+            IocManager.IocContainer.Kernel.ComponentRegistered += Kernel_ComponentRegistered;
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+            IocManager.Register<IMultiTenantLocalizationDictionary, MultiTenantLocalizationDictionary>(DependencyLifeStyle.Transient);
+        }
+
+        private void Kernel_ComponentRegistered(string key, Castle.MicroKernel.IHandler handler)
+        {
+            if (typeof (IAbpZeroFeatureValueStore).IsAssignableFrom(handler.ComponentModel.Implementation) && !IocManager.IsRegistered<IAbpZeroFeatureValueStore>())
+            {
+                IocManager.IocContainer.Register(
+                    Component.For<IAbpZeroFeatureValueStore>().ImplementedBy(handler.ComponentModel.Implementation).Named("AbpZeroFeatureValueStore").LifestyleTransient()
+                    );
+            }
+        }
+    }
+}
---
+++ src/Tests/Abp.Zero.SampleApp/Features/FeatureValueStore.cs
@@ -0,0 +1,28 @@
+﻿using Abp.Application.Features;
+using Abp.Domain.Repositories;
+using Abp.MultiTenancy;
+using Abp.Runtime.Caching;
+using Abp.Zero.SampleApp.MultiTenancy;
+using Abp.Zero.SampleApp.Roles;
+using Abp.Zero.SampleApp.Users;
+
+namespace Abp.Zero.SampleApp.Features
+{
+    public class FeatureValueStore : AbpFeatureValueStore<Tenant, Role, User>
+    {
+        public FeatureValueStore(ICacheManager cacheManager,
+            IRepository<TenantFeatureSetting, long> tenantFeatureRepository,
+            IRepository<Tenant> tenantRepository,
+            IRepository<EditionFeatureSetting, long> editionFeatureRepository,
+            IFeatureManager featureManager)
+            : base(
+                  cacheManager, 
+                  tenantFeatureRepository, 
+                  tenantRepository, 
+                  editionFeatureRepository, 
+                  featureManager)
+        {
+
+        }
+    }
+}
